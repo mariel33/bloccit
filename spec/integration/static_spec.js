5b6db399a7992022dafcbb9bb4145e07ab1b717f
Mariel <mariel.capulli@gmail.com>
2018-06-14 21:49:17 -0400

    express assignment complete

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -5,11 +5,20 @@ const base = "http://localhost:3000/";
 describe("routes: static", () => {
 
     describe("GET /", () => {
-        it("should return status code 200", (done) => {
+        it("should return status code 200", () => {
             request.get(base, (err, res, body) => {
                 expect(res.statusCode).toBe(200);
-                done();
             });
         });
     });
-})
+
+    describe('GET /marco', () => {
+        it('should return status code 200 and body should contain polo', () => {
+            request.get('http://localhost:3000/marco', (err, res, body) => {
+                expect(res.statusCode).toBe(200);
+                expect(res.body).toBe('Polo');
+            });
+        });
+    });
+});
+
