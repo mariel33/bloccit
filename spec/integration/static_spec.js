9594321d04d1e524a0da10f8c602cc394767d5e9
Mariel <mariel.capulli@gmail.com>
2018-06-16 12:37:03 -0400

    Added About Us

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -5,11 +5,21 @@ const base = "http://localhost:3000/";
 describe("routes: static", () => {
 
     describe("GET /", () => {
-        it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", () => {
+        it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", (done) => {
             request.get(base, (err, res, body) => {
                 expect(res.statusCode).toBe(200);
                 expect(body).toContain("Welcome to Bloccit");
+                done();
             });
         });
     });
+
+    describe("GET /about", () => {
+        it("should contain the string 'About Us'", (done) => {
+            request.get("http://localhost:3000/about", (err, res, body) => {
+                expect(body).toContain("About Us");
+                done();
+            })
+        })
+    })
 })
