c110fb136d4c9199627c3d06a8828369f56a09bc
Mariel <mariel.capulli@gmail.com>
2018-06-19 05:58:58 -0400

    Topic Resource1 Checkpoint

--- spec/integration/topics_spec.js
+++ spec/integration/topics_spec.js
@@ -2,10 +2,10 @@ const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/topics/";
 const sequelize = require("../../src/db/models/index").sequelize;
-const Topic = require("../../src/db/models").Topic;
+const Topic = require("../../src/db/models/topic").Topic;
 
 describe("routes: topics", () => {
-    beforeEach((done) => {
+    beforeEach(() => {
         this.topic;
         sequelize.sync({ force: true }).then((res) => {
 
@@ -15,7 +15,7 @@ describe("routes: topics", () => {
             })
                 .then((topic) => {
                     this.topic = topic;
-                    done();
+                    //done();
                 })
                 .catch((err) => {
                     console.log(err);
@@ -27,20 +27,16 @@ describe("routes: topics", () => {
     });
 
     describe("GET /topics", () => {
-        it("should return a status code 200 and all topics", (done) => {
+        it("should return a status code 200 and all topics", () => {
 
             //#3
             request.get(base, (err, res, body) => {
                 expect(res.statusCode).toBe(200);
                 expect(err).toBeNull();
-                expect(body).toContain("Topics");
+                expect(body).toContain("topics");
                 expect(body).toContain("JS Frameworks");
-                done();
+                //done();
             });
         })
-        .catch((err) => {
-            console.log(err);
-            done();
-        }, 30000);
     });
 });
