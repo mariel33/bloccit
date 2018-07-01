36fa707b3aa6bcf9145fe2394d2fc152c0c26422
Mariel <mariel.capulli@gmail.com>
2018-07-01 15:02:38 -0400

    Add authorization

--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -1,53 +1,24 @@
 const Topic = require("./models").Topic;
 const Post = require("./models").Post;
+const Authorizer = require("../policies/topic")
 
 module.exports = {
 
-  //#1
-
-  //#1
-  getAllTopics(callback) {
+  getAllTopics(callback){
     return Topic.all()
 
-      //#2
-      .then((topics) => {
-        callback(null, topics);
-      })
-      .catch((err) => {
-        callback(err);
-      })
+    .then((topics) => {
+      callback(null, topics);
+    })
+    .catch((err) => {
+      callback(err);
+    })
   },
 
-  addTopic(newTopic, callback) {
+  addTopic(newTopic, callback){
     return Topic.create({
       title: newTopic.title,
       description: newTopic.description
-    })
-      .then((topic) => {
-        callback(null, topic);
-      })
-      .catch((err) => {
-        callback(err);
-      })
-  },
-
-  getTopic(id, callback) {
-    return Topic.findById(id, {
-      include: [{
-        model: Post,
-        as: "posts"
-      }]
-    }).then((topic) => {
-      callback(null, topic);
-    })
-      .catch((err) => {
-        console.log(err);
-      })
-  },
-
-  deleteTopic(id, callback) {
-    return Topic.destroy({
-      where: {id}
     })
     .then((topic) => {
       callback(null, topic);
@@ -57,23 +28,73 @@ module.exports = {
     })
   },
 
-  updateTopic(id, updatedTopic, callback){
-    return Topic.findById(id)
+  getTopic(id, callback){
+    return Topic.findById(id, {
+      include: [{
+        model: Post,
+        as: "posts"
+      }]
+    })
     .then((topic) => {
-      if(!topic){
-        return callback("Topic not found");
-      }
+      callback(null, topic);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  },
 
-      topic.update(updatedTopic, {
-        fields: Object.keys(updatedTopic)
-      })
-      .then(() => {
-        callback(null, topic);
-      })
-      .catch((err) => {
-        callback(err);
-      });
-    });
-  }
+  deleteTopic(req, callback){
 
-}
+        return Topic.findById(req.params.id)
+        .then((topic) => {
+   
+          const authorized = new Authorizer(req.user, topic).destroy();
+   
+          if(authorized) {
+            topic.destroy()
+            .then((res) => {
+              callback(null, topic);
+            });
+            
+          } else {
+   
+            req.flash("notice", "You are not authorized to do that.")
+            callback(401);
+          }
+        })
+        .catch((err) => {
+          callback(err);
+        });
+      },
+
+   updateTopic(req, updatedTopic, callback){
+
+         return Topic.findById(req.params.id)
+         .then((topic) => {
+        
+            if(!topic){
+             return callback("Topic not found");
+           }
+        
+            const authorized = new Authorizer(req.user, topic).update();
+    
+           if(authorized) {
+        
+              topic.update(updatedTopic, {
+               fields: Object.keys(updatedTopic)
+             })
+             .then(() => {
+               callback(null, topic);
+              })
+              .catch((err) => {
+               callback(err);
+             });
+           } else {
+        
+              req.flash("notice", "You are not authorized to do that.");
+             callback("Forbidden");
+           }
+          });
+       }
+
+};
